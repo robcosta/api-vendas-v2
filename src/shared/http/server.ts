774e5448f91542ee6d6273512254121e32eb7495
Roberto Costa <robertocosta66@gmail.com>
2021-10-26 16:30:49 -0300

    server ok

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes/index';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333! 🙌');
+});
