4f0a3cebee6360eef55bb07d0fce3f48a7659e01
Roberto Costa <robertocosta66@gmail.com>
2021-10-26 19:42:48 -0300

    Config Error

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Dev! ' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello Dev! ' }));
 
 export default routes;
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import routes from './routes/index';
+
 const app = express();
 
 app.use(cors());
