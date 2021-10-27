ea14c35d8e61634d96a5ca78be3d4d262b60586c
Roberto Costa <robertocosta66@gmail.com>
2021-10-27 16:24:38 -0300

    Products routs

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
+//import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes/index';
 import AppError from '@shared/errors/AppError';
@@ -11,19 +12,21 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.use((error: Error, request: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      status: 'error',
-      message: error.message,
-    });
-  }
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
 
-  return res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-});
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 🙌');
