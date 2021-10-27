0db42ba0e12db846d9e475a2d2f4181818a43f60
Roberto Costa <robertocosta66@gmail.com>
2021-10-27 08:15:36 -0300

    First Migration

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,12 +1,29 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes/index';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm'; //como  arquivo é index.ts não precisa do nome
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use((error: Error, request: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(3333, () => {
   console.log('Server started on port 3333! 🙌');
---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,3 @@
+import { createConnection } from 'typeorm';
+//Busca pelo 'ormconfig.json'
+createConnection();
