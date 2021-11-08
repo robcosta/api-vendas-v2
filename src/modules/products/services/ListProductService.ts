8951813b40a8a02d05bdf2c7dc64e1b25a27a6e7
Roberto Costa <robertocosta66@gmail.com>
2021-11-08 16:31:53 -0300

    Pagination products

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,20 +1,20 @@
-import { getCustomRepository } from 'typeorm';
-import Product from '../typeorm/entities/Product';
-import ProductRepository from '../typeorm/repositories/ProductsRepository';
 import RedisCache from '@shared/cache/RedisCache';
+import { getCustomRepository } from 'typeorm';
+import { PaginationAwareObject } from 'typeorm-pagination/dist/helpers/pagination';
+import ProductRepository from '../typeorm/repositories/ProductsRepository';
 
 class ListProductService {
-  public async execute(): Promise<Product[]> {
+  public async execute(): Promise<PaginationAwareObject> {
     const productsRepository = getCustomRepository(ProductRepository);
 
     const redisCache = new RedisCache();
 
-    let products = await redisCache.recover<Product[]>(
+    let products = await redisCache.recover<PaginationAwareObject>(
       'api-vendas-PRODUCT_LIST',
     );
 
     if (!products) {
-      products = await productsRepository.find();
+      products = await productsRepository.createQueryBuilder().paginate();
       await redisCache.save('api-vendas-PRODUCT_LIST', products);
     }
 
