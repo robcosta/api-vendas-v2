3fcf9b8274d45cdd56227d7e6b05bcdf2f3018e4
Roberto Costa <robertocosta66@gmail.com>
2021-11-08 16:35:42 -0300

    Paginated Users

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -1,12 +1,13 @@
 import { getCustomRepository } from 'typeorm';
+import { PaginationAwareObject } from 'typeorm-pagination/dist/helpers/pagination';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 
 class ListUserService {
-  public async execute(): Promise<User[]> {
+  public async execute(): Promise<PaginationAwareObject> {
     const usersRepository = getCustomRepository(UsersRepository);
 
-    const users = await usersRepository.find();
+    const users = await usersRepository.createQueryBuilder().paginate();
 
     return users;
   }
