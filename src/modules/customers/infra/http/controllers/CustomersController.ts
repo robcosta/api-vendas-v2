847180734817eec8dab9daf6baa5aee3b56124d2
Roberto Costa <robertocosta66@gmail.com>
2021-11-23 17:40:23 -0300

    Dependency Invertion

--- src/modules/customers/infra/http/controllers/CustomersController.ts
+++ src/modules/customers/infra/http/controllers/CustomersController.ts
@@ -4,6 +4,7 @@ import DeleteCustomerService from '../../../services/DeleteCustomerService';
 import ListCustomerService from '../../../services/ListCustomerService';
 import ShowCustomerService from '../../../services/ShowCustomerService';
 import UpdateCustomerService from '../../../services/UpdateCustomerService';
+import CustomersRepository from '../../typeorm/repositories/CustomersRepository';
 
 class CustomersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -23,7 +24,8 @@ class CustomersController {
 
   public async create(request: Request, response: Response): Promise<Response> {
     const { name, email } = request.body;
-    const createCustomer = new CreateCustomerService();
+    const customersRepository = new CustomersRepository();
+    const createCustomer = new CreateCustomerService(customersRepository);
     const customer = await createCustomer.execute({ name, email });
 
     return response.json(customer);
--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -1,30 +1,22 @@
 import redisCache from '@shared/cache/RedisCache';
 import AppError from '@shared/errors/AppError';
-import { getCustomRepository } from 'typeorm';
-import Customer from '../infra/typeorm/entities/Customer';
-import CustomersRepository from '../infra/typeorm/repositories/CustomersRepository';
-
-interface IRequest {
-  name: string;
-  email: string;
-}
+import { ICreateCustomer } from '../domain/models/ICreateCustomer';
+import { ICustomer } from '../domain/models/ICustomer';
+import { ICustomersRepository } from '../domain/repositories/ICustomersRepository';
 
 class CreateCustomerService {
-  public async execute({ name, email }: IRequest): Promise<Customer> {
-    const customersRepository = getCustomRepository(CustomersRepository);
-    const emailExist = await customersRepository.findByEmail(email);
+  constructor(private customersRepository: ICustomersRepository) {}
+
+  public async execute({ name, email }: ICreateCustomer): Promise<ICustomer> {
+    const emailExist = await this.customersRepository.findByEmail(email);
 
     if (emailExist) {
       throw new AppError('Email address already used.');
     }
 
-    // const redisCache = new RedisCache();
-
-    const customer = customersRepository.create({ name, email });
+    const customer = await this.customersRepository.create({ name, email });
 
     await redisCache.invalidate('api-vendas-CUSTOMER_LIST');
-
-    await customersRepository.save(customer);
 
     return customer;
   }
